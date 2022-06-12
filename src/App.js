7e691ceba245fa8c0aaede8d7b09df96a569daa0
smishra-facilgo <smishra@anuktaitsolutions.com>
2022-06-12 23:42:21 +0530

    Image app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React ,{useState} from 'react';
 import './App.css';
 
 function App() {
+  //ndF2dFSdAiFHdTjBLG9fGYL3bLzR-iS4OcIEWrcrrYM  Access Key
+  //UYidaMBLCN6ONE3Q_Sr8UrAsaaMOIyj5ZDMkbIHyJzw  secret key
+  //https://api.unsplash.com/photos/?client_id=ndF2dFSdAiFHdTjBLG9fGYL3bLzR-iS4OcIEWrcrrYM
+  ///search/photos
+  //https://api.unsplash.com/photos/search/photos?client_id=ndF2dFSdAiFHdTjBLG9fGYL3bLzR-iS4OcIEWrcrrYM
+  const [searchValue,setsearchValue] = useState("");
+  const [result,setresult]=useState([]);
+  const imgPerPage =100;
+  const fetchImages = () =>{
+    fetch(`https://api.unsplash.com/search/photos?client_id=ndF2dFSdAiFHdTjBLG9fGYL3bLzR-iS4OcIEWrcrrYM&query=${searchValue}&per_page=${imgPerPage}`)
+    .then(response =>response.json())
+    .then(data=>{
+      setresult(data.results)
+      console.log(data.results)})
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       <div className='my-div'>
+        <span>Search </span>
+        <input 
+          type="text"
+          style={{width:"60%" ,height:"30%",size:"20",padding:"5px"}} 
+          value={searchValue}
+          onChange = {(e)=>setsearchValue(e.target.value)}
+
+        />
+        <button className='submitbtn' onClick={()=>fetchImages()}>Send</button>
+       </div>
+       <div className='gallery'>
+        {
+          result.map((item)=>{
+            return <img className='item' key={item.id} src = {item.urls.regular}  />;
+          })
+        }
+       </div>
     </div>
   );
 }
